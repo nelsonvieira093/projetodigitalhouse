31003ce35a051a8093b21e3cd48ce7953c30112c
andersonfmonteiro <andersonfmonteiro@gmail.com>
2022-10-26 19:43:32 -0300

    meu terceiro commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const usuarios = require('./database/users.json')
-const listarIdadeMaior = () => usuarios.filter((usuario) => usuario.idade > 2)  
+const listarIdadeMaior = () => usuarios.filter((usuario) => usuario.idade > 18)  
 
 console.log(listarIdadeMaior())
 
